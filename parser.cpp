3c76b1c48ad21d2583b850f34ea0c416226c50f5
SumanShahzad123 <144985287+SumanShahzad123@users.noreply.github.com>
2024-11-22 17:44:21 +0500

    New change for line error

--- parser.cpp
+++ parser.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 enum TokenType {
-    T_INT, T_ID, T_NUM, T_IF, T_ELSE, T_RETURN, 
+    T_INT, T_FLOAT, T_DOUBLE, T_STRING, T_BOOL, T_CHAR, T_ID, T_NUM, T_IF, T_ELSE, T_RETURN, 
     T_ASSIGN, T_PLUS, T_MINUS, T_MUL, T_DIV, 
     T_LPAREN, T_RPAREN, T_LBRACE, T_RBRACE,  
     T_SEMICOLON, T_GT, T_EOF, 
@@ -62,6 +62,11 @@ class Lexer {
                 if (isalpha(current)) {
                     string word = consumeWord();
                     if (word == "int") tokens.push_back(Token{T_INT, word, line});
+                    else if (word == "float") tokens.push_back(Token{T_FLOAT, word, line});
+                    else if (word == "double") tokens.push_back(Token{T_DOUBLE, word, line});
+                    else if (word == "string") tokens.push_back(Token{T_STRING, word, line});
+                    else if (word == "bool") tokens.push_back(Token{T_BOOL, word, line});
+                    else if (word == "char") tokens.push_back(Token{T_CHAR, word, line});
                     else if (word == "if") tokens.push_back(Token{T_IF, word, line});
                     else if (word == "else") tokens.push_back(Token{T_ELSE, word, line});
                     else if (word == "return") tokens.push_back(Token{T_RETURN, word, line});
@@ -94,8 +99,14 @@ class Lexer {
 
         string consumeNumber() {
             size_t start = pos;
-            while (pos < src.size() && isdigit(src[pos])) pos++;
-            return src.substr(start, pos - start);
+            bool isFloat = false;
+            while (pos < src.size() && (isdigit(src[pos]) || src[pos] == '.')) {
+                if (src[pos] == '.') isFloat = true;
+                pos++;
+            }
+            if (isFloat)
+                return src.substr(start, pos - start);  // Will return float values as strings
+            return src.substr(start, pos - start);      // Will return integer numbers as strings
         }
 
         string consumeWord() {
@@ -125,11 +136,13 @@ public:
 private:
     vector<Token> tokens;
     size_t pos;
+    map<string, TokenType> symbolTable;
 
     void parseStatement() {
-        if (tokens[pos].type == T_INT) {
+        if (tokens[pos].type == T_INT || tokens[pos].type == T_FLOAT || tokens[pos].type == T_DOUBLE || 
+            tokens[pos].type == T_STRING || tokens[pos].type == T_BOOL || tokens[pos].type == T_CHAR) {
             parseDeclaration();
-        } else if (tokens[pos].type == T_ID) {
+        }else if (tokens[pos].type == T_ID) {
             parseAssignment();
         } else if (tokens[pos].type == T_IF) {
             parseIfStatement();
@@ -146,6 +159,11 @@ private:
     switch (type) {
         case T_ID: return "identifier";
         case T_INT: return "int";
+        case T_FLOAT: return "float";
+        case T_DOUBLE: return "double";
+        case T_STRING: return "string";
+        case T_BOOL: return "bool";
+        case T_CHAR: return "char";
         case T_NUM: return "number";
         case T_IF: return "if";
         case T_ELSE: return "else";
@@ -173,17 +191,82 @@ private:
         }
         expect(T_RBRACE);  
     }
-    void parseDeclaration() {
-        expect(T_INT);
-        expect(T_ID);
+     void parseDeclaration() {
+        TokenType declaredType = tokens[pos].type;  // Save the type of variable
+        expect(tokens[pos].type);  // Expect the data type (int, float, etc.)
+        string varName = tokens[pos].value;
+        expect(T_ID);  // Expect an identifier (variable name)
+        
+        if (symbolTable.find(varName) != symbolTable.end()) {
+            cout << "Error: Variable '" << varName << "' already declared on line " << tokens[pos].line << endl;
+            exit(1);
+        }
+
+        symbolTable[varName] = declaredType;  // Add the variable and its type to the symbol table
         expect(T_SEMICOLON);
     }
 
-    void parseAssignment() {
+     void parseAssignment() {
+        string varName = tokens[pos].value;
         expect(T_ID);
+
+        if (symbolTable.find(varName) == symbolTable.end()) {
+            cout << "Error: Variable '" << varName << "' not declared before assignment on line " << tokens[pos].line << endl;
+            exit(1);
+        }
+
+        TokenType expectedType = symbolTable[varName];  // Get the declared type of the variable
         expect(T_ASSIGN);
-        parseExpression();
+        TokenType exprType = parseExpression();  // Get the type of the assigned expression
+
+        // Check for type mismatch
+        if (expectedType == T_INT && exprType == T_FLOAT) {
+            cout << "Error: Cannot assign float value to an int variable '" << varName << "' on line " << tokens[pos].line << endl;
+            exit(1);
+        }
+
         expect(T_SEMICOLON);
+    }
+    TokenType parseExpression() {
+        TokenType termType = parseTerm();
+        while (tokens[pos].type == T_PLUS || tokens[pos].type == T_MINUS) {
+            pos++;
+            parseTerm();  // Here, we assume the expression returns the correct type (for simplicity)
+        }
+        return termType;
+    }
+    TokenType parseTerm() {
+        TokenType factorType = parseFactor();
+        while (tokens[pos].type == T_MUL || tokens[pos].type == T_DIV) {
+            pos++;
+            parseFactor();  // We assume the term returns the correct type (for simplicity)
+        }
+        return factorType;
+    }
+
+    TokenType parseFactor() {
+        if (tokens[pos].type == T_NUM) {
+            string value = tokens[pos].value;
+            pos++;
+            return (value.find('.') != string::npos) ? T_FLOAT : T_INT;  // Return float if it contains a dot
+        } else if (tokens[pos].type == T_ID) {
+            string varName = tokens[pos].value;
+            if (symbolTable.find(varName) == symbolTable.end()) {
+                cout << "Error: Variable '" << varName << "' not declared on line " << tokens[pos].line << endl;
+                exit(1);
+            }
+            TokenType varType = symbolTable[varName];
+            pos++;
+            return varType;
+        } else if (tokens[pos].type == T_LPAREN) {
+            expect(T_LPAREN);
+            TokenType exprType = parseExpression();
+            expect(T_RPAREN);
+            return exprType;
+        } else {
+            cout << "Syntax error: unexpected token " << tokens[pos].value << endl;
+            exit(1);
+        }
     }
 
     void parseIfStatement() {
@@ -194,7 +277,7 @@ private:
         parseStatement();  
         if (tokens[pos].type == T_ELSE) {
             expect(T_ELSE);
-            parseStatement();  
+            parseStatement();
         }
     }
 
@@ -204,49 +287,24 @@ private:
         expect(T_SEMICOLON);
     }
 
-    void parseExpression() {
-        parseTerm();
-        while (tokens[pos].type == T_PLUS || tokens[pos].type == T_MINUS) {
-            pos++;
-            parseTerm();
+    void parseBlock() {
+        expect(T_LBRACE);
+        while (tokens[pos].type != T_RBRACE && tokens[pos].type != T_EOF) {
+            parseStatement();
         }
-        if (tokens[pos].type == T_GT) {
-            pos++;
-            parseExpression();  // After relational operator, parse the next expression
-        }
-    }
-
-    void parseTerm() {
-        parseFactor();
-        while (tokens[pos].type == T_MUL || tokens[pos].type == T_DIV) {
-            pos++;
-            parseFactor();
-        }
-    }
-
-    void parseFactor() {
-        if (tokens[pos].type == T_NUM || tokens[pos].type == T_ID) {
-            pos++;
-        } else if (tokens[pos].type == T_LPAREN) {
-            expect(T_LPAREN);
-            parseExpression();
-            expect(T_RPAREN);
-        } else {
-            cout << "Syntax error: unexpected token " << tokens[pos].value << endl;
-            exit(1);
-        }
+        expect(T_RBRACE);
     }
 
     void expect(TokenType type) {
-    if (tokens[pos].type == type) {
-        pos++;
-    } else {
-        cout << "Syntax error: expected " << tokenTypeToString(type)
-             << " but found '" << tokens[pos].value
-             << "' on line " << tokens[pos].line << endl;
-        exit(1);
+        if (tokens[pos].type == type) {
+            pos++;
+        } else {
+            cout << "Syntax error: expected " << tokenTypeToString(type)
+                 << " but found '" << tokens[pos].value
+                 << "' on line " << tokens[pos].line << endl;
+            exit(1);
+        }
     }
-}
 
 };
 
@@ -265,12 +323,8 @@ int main(int argc, char* argv[]) {
     string input((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
     file.close();
 
-
     Lexer lexer(input);
     vector<Token> tokens = lexer.tokenize();
-    
-    
-
     Parser parser(tokens);
     parser.parseProgram();
 
